e730b65170fd100afe1fa4d2815d3033e174ab4d
Pavel Gordienko <guzmo@yandex.ru>
2025-07-06 00:55:38 +0300

    fix: deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://test-bee-jee.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://test-bee-jee.vercel.app'],
     credentials: true
 }));
 
